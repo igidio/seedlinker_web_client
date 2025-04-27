8339f1d8c2931f2d4d2d1082425b9159bb971d8f
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-04-27 16:24:32 -0400

    create avatar component

---
+++ src/components/ui/UiAvatar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="avatar">
+    <div class="ring-base-content ring-offset-base-100 w-10 rounded-full ring ring-offset-2">
+      <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    </div>
+  </div>
+</template>
