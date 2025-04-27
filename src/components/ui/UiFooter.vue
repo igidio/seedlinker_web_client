9509c9de28b672d4babd6feefea062107146b6a7
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-04-27 16:24:58 -0400

    create footer component

---
+++ src/components/ui/UiFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-row items-center justify-center">
+  <span class="font-bold">{{ current }} igid.io</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+const current = new Date().getFullYear()
+</script>
