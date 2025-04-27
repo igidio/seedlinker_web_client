4dd77dfd8f69953853b05c3b682e1f4c8a486081
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-04-27 16:24:50 -0400

    create header and logo components

---
+++ src/components/ui/UiHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-row justify-between items-center">
+    <div class="flex flex-row items-center gap-2">
+    <UiLogo/>
+    <span class="font-bold text-3xl">Seedlinker</span>
+    </div>
+
+    <UiDropdown>
+      <UiAvatar/>
+    </UiDropdown>
+  </div>
+</template>
+<script setup lang="ts">
+import UiLogo from '@/components/ui/UiLogo.vue'
+import UiAvatar from '@/components/ui/UiAvatar.vue'
+import UiDropdown from '@/components/ui/UiDropdown.vue'
+</script>
---
+++ src/components/ui/UiLogo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-auto w-auto relative items-center">
+    <div class="w-8 h-12">
+      <div class="logo bg-base-content" />
+    </div>
+  </div>
+</template>
+
+<style>
+@reference "tailwindcss";
+.logo {
+  @apply h-full;
+  -webkit-mask-size: cover;
+  mask-size: cover;
+  mask: url(/svg/logo.svg) no-repeat 25% 25%;
+}
+</style>
