1358bbd14508d5096e3a6cd75c7abe59ebc69cda
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-04-30 11:41:58 -0400

    create login view ui

---
+++ src/components/project/auth/LoginForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <UiCard>
+    <template #top> </template>
+    <template #default>
+      <div class="flex flex-col gap-2">
+        <h2 class="font-black text-xl text-center">{{ $t('login.title') }}</h2>
+        <p class="text-center">{{ $t('login.description') }}</p>
+
+        <fieldset class="fieldset">
+          <legend class="fieldset-legend">{{ $t('login.fields.username') }}</legend>
+          <input type="text" class="input w-full" />
+        </fieldset>
+
+        <fieldset class="fieldset">
+          <legend class="fieldset-legend">{{ $t('login.fields.password') }}</legend>
+          <input type="password" class="input w-full" />
+        </fieldset>
+        <button class="btn btn-lg btn-primary block mt-2">{{ $t('login.button') }}</button>
+        <i18n-t
+          keypath="login.signup_redirection.text"
+          tag="label"
+          for="login.signup_redirection.link"
+          class="text-end mt-1"
+        >
+          <RouterLink to="signup" class="font-bold"
+            >{{ $t(`login.signup_redirection.link`) }}
+          </RouterLink>
+        </i18n-t>
+      </div>
+    </template>
+  </UiCard>
+</template>
+
+<script setup lang="ts">
+import UiCard from '@/components/ui/UiCard.vue'
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <div class="flex flex-col items-center gap-2 mb-4">
+    <UiLogo :size="52"/>
+    <span class="font-bold text-3xl">Seedlinker</span>
+  </div>
+
+
+  <div class="flex flex-row justify-center items-center ">
+    <LoginForm/>
+  </div>
+
+
+
+</template>
+<script setup lang="ts">
+import LoginForm from '@/components/project/auth/LoginForm.vue'
+import UiLogo from '@/components/ui/UiLogo.vue'
+</script>
