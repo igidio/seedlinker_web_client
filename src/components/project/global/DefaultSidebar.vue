dc1c427cc49ef6bb57f5ab837ba953f114694414
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-04-30 23:50:19 -0400

    create sidebar and make responsive

---
+++ src/components/project/global/DefaultSidebar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="hidden md:block h-64 w-64 bg-white">
+    This is the sidebar
+  </div>
+</template>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -2,9 +2,14 @@
   <div class="main">
     <div>
       <UiHeader/>
+      <div class="flex flex-row gap-4">
+      <DefaultSidebar/>
+        <div class="w-full">
       <Suspense fallback="">
         <slot/>
       </Suspense>
+        </div>
+      </div>
     </div>
 
     <UiFooter/>
@@ -13,4 +18,5 @@
 <script setup lang="ts">
 import UiHeader from '@/components/ui/UiHeader.vue'
 import UiFooter from '@/components/ui/UiFooter.vue'
+import DefaultSidebar from '@/components/project/global/DefaultSidebar.vue'
 </script>
