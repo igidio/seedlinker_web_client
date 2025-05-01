f34a80a4b6163b4f9460cc1057aa530c1b4d7ab3
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-04-30 23:39:44 -0400

    update styles and header component

--- src/components/ui/UiHeader.vue
+++ src/components/ui/UiHeader.vue
@@ -5,21 +5,20 @@
       <span class="font-bold text-3xl">Seedlinker</span>
     </router-link>
 
-    <!--
-        <div>
-        <UiDropdown>
-          <UiAvatar/>
-          <template #dropdown>
-            <li>
-              <a><Icon icon="ph:user-fill"/>Modificar perfil</a>
-            </li>
-            <li><a><Icon icon="ph:door-duotone"/>Cerrar sesión</a></li>
-          </template>
-        </UiDropdown>
-        </div>
-        -->
 
-    <div class="flex flex-row items-center gap-2">
+    <div v-if="is_authenticated">
+    <UiDropdown>
+      <UiAvatar/>
+      <template #dropdown>
+        <li>
+          <a><Icon icon="ph:user-fill"/>Modificar perfil</a>
+        </li>
+        <li><button @click="configStore.logout"><Icon icon="ph:door-duotone"/>Cerrar sesión</button></li>
+      </template>
+    </UiDropdown>
+    </div>
+
+    <div class="flex flex-row items-center gap-2" v-else>
       <router-link :to="{ name: 'login'}" class="btn btn-ghost">{{ $t('login.title') }}</router-link>
       <router-link :to="{ name: 'signup'}" class="btn btn-neutral">{{ $t('signup.title') }}</router-link>
     </div>
@@ -30,4 +29,11 @@ import UiLogo from '@/components/ui/UiLogo.vue'
 import UiAvatar from '@/components/ui/UiAvatar.vue'
 import UiDropdown from '@/components/ui/UiDropdown.vue'
 import { Icon } from '@iconify/vue'
+import { useConfigStore } from '@/stores/config.store.ts'
+import { storeToRefs } from 'pinia'
+
+
+
+const configStore = useConfigStore()
+const { is_authenticated } = storeToRefs(configStore)
 </script>
