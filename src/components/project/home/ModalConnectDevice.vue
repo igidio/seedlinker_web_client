42a87fff9f9b57ab0a1a867b3bd436f06b5df407
Salvador Cáceres <salvadorcaceresc@gmail.com>
2025-05-03 01:15:20 -0400

    execute method when modal is close

--- src/components/project/home/ModalConnectDevice.vue
+++ src/components/project/home/ModalConnectDevice.vue
@@ -1,6 +1,14 @@
 <template>
-  <form>
-    <UiModal v-model="trigger" :title="data.is_new ? $t('home.modal_connect_device.manual.title') : $t('home.modal_connect_device.auto.title', [data.type])">
+  <form @submit.prevent="submit">
+    <UiModal
+      v-model="trigger"
+      @close="() => {new_uuid = '';error_message = null;}"
+      :title="
+        data.is_new
+          ? $t('home.modal_connect_device.manual.title')
+          : $t('home.modal_connect_device.auto.title', [data.type])
+      "
+    >
       <template v-slot:default>
         <div class="flex flex-col w-full" v-if="data.is_new">
           <p>{{ $t('home.modal_connect_device.manual.description') }}</p>
@@ -18,10 +26,13 @@
           <p>{{ $t('home.modal_connect_device.auto.description') }}</p>
           <p class="font-bold">{{ data.uuid }}</p>
         </div>
+        <p class="text-error text-end" v-if="error_message">{{ $t(error_message) }}</p>
       </template>
       <template v-slot:footer>
         <button class="btn" type="button" @click="trigger?.close">{{ $t('close') }}</button>
-        <button class="btn btn-neutral" :disabled="disable_save_button">{{ $t('save') }}</button>
+        <button class="btn btn-neutral" :disabled="disable_save_button || is_loading">
+          {{ $t('save') }}
+        </button>
       </template>
     </UiModal>
   </form>
@@ -30,8 +41,11 @@
 import UiModal from '@/components/ui/UiModal.vue'
 import { computed, ref } from 'vue'
 import { is_valid_uuid } from '@/utils/validate_uuid.ts'
+import { api_client } from '@/utils/axios.ts'
+import type { AxiosError } from 'axios'
 
 const trigger = defineModel<HTMLDialogElement>()
+const error_message = ref<string | null>(null)
 
 const props = defineProps<{
   data: {
@@ -40,9 +54,37 @@ const props = defineProps<{
     is_new: boolean
   }
 }>()
+
+
+
 const new_uuid = ref('')
+const is_loading = ref(false)
 
 const disable_save_button = computed(() => {
-  return props.data.is_new && !is_valid_uuid(new_uuid.value);
+  return props.data.is_new && !is_valid_uuid(new_uuid.value)
 })
+
+const submit = async () => {
+  const uuid_to_submit = props.data.is_new ? new_uuid.value.trim() : props.data.uuid
+  console.log(uuid_to_submit)
+  is_loading.value = true
+  if (!is_valid_uuid(uuid_to_submit)) {
+    error_message.value = 'error.form.invalid_uuid'
+    is_loading.value = false
+    return
+  }
+  await api_client
+    .post(`/devices/${uuid_to_submit}`)
+    .then(() => {
+      trigger.value?.close()
+      new_uuid.value = ''
+      error_message.value = null
+    })
+    .catch((e: AxiosError) => {
+      error_message.value = (e.response?.data as { detail?: string })?.detail || null
+    })
+    .finally(() => {
+      is_loading.value = false
+    })
+}
 </script>
