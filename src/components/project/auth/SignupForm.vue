812c5e78c61bc52d8e7aa121fed9e8a59dc30773
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2025-05-20 11:42:17 -0400

    redirect with tokens on signup

--- src/components/project/auth/SignupForm.vue
+++ src/components/project/auth/SignupForm.vue
@@ -7,33 +7,59 @@
 
         <fieldset class="fieldset">
           <legend class="fieldset-legend">{{ $t('signup.fields.username') }}</legend>
-          <input type="text" class="input w-full" autocomplete="username" v-model="form.username" @input="validate_inputs"/>
+          <input
+            type="text"
+            class="input w-full"
+            autocomplete="username"
+            v-model="form.username"
+            @input="validate_inputs"
+          />
         </fieldset>
 
         <fieldset class="fieldset">
           <legend class="fieldset-legend">{{ $t('signup.fields.email') }}</legend>
-          <input type="email" class="input w-full" autocomplete="email"  v-model="form.email" @input="validate_inputs"/>
+          <input
+            type="email"
+            class="input w-full"
+            autocomplete="email"
+            v-model="form.email"
+            @input="validate_inputs"
+          />
         </fieldset>
 
         <fieldset class="fieldset">
           <legend class="fieldset-legend">{{ $t('signup.fields.password') }}</legend>
-          <input type="password" class="input w-full" autocomplete="new_password" v-model="form.password" @input="validate_inputs"/>
+          <input
+            type="password"
+            class="input w-full"
+            autocomplete="new_password"
+            v-model="form.password"
+            @input="validate_inputs"
+          />
         </fieldset>
         <fieldset class="fieldset">
           <legend class="fieldset-legend">{{ $t('signup.fields.confirm_password') }}</legend>
-          <input type="password" class="input w-full" autocomplete="new_password" v-model="form.confirm_password" @input="validate_inputs"/>
+          <input
+            type="password"
+            class="input w-full"
+            autocomplete="new_password"
+            v-model="form.confirm_password"
+            @input="validate_inputs"
+          />
         </fieldset>
         <p class="text-error text-center" v-if="error_message">{{ $t(error_message) }}</p>
-        <button class="btn btn-lg btn-primary block mt-2 flex flex-row" :disabled="is_loading"><Icon icon="ph:door-open-fill"/>{{ $t('signup.button') }}</button>
+        <button class="btn btn-lg btn-primary block mt-2 flex flex-row" :disabled="is_loading">
+          <Icon icon="ph:door-open-fill" />{{ $t('signup.button') }}
+        </button>
         <i18n-t
           keypath="signup.terms_and_conditions.text"
           tag="label"
           scope="global"
           for="signup.terms_and_conditions.text"
-          class=" text-center"
+          class="text-center"
         >
           <RouterLink to="signup" class="font-bold"
-          >{{ $t(`signup.terms_and_conditions.link`) }}
+            >{{ $t(`signup.terms_and_conditions.link`) }}
           </RouterLink>
         </i18n-t>
       </form>
@@ -47,6 +73,7 @@ import { reactive, ref } from 'vue'
 import { z } from 'zod'
 import { AxiosError } from 'axios'
 import { api_client } from '@/utils/axios.ts'
+import { set_cookie } from '@/utils/cookie.ts'
 
 const error_message = ref<string | null>(null)
 const is_loading = ref(false)
@@ -57,14 +84,24 @@ const form = reactive({
   confirm_password: '',
 })
 
-const schema = z.object({
-  username: z.string({ required_error: 'error.input.username.required' }).min(3, { message: 'error.input.username.min_length' }),
-  email: z.string({ required_error: 'error.input.email.required' }).email({ message: 'error.input.email.invalid' }),
-  password: z.string({ required_error: 'error.input.password.required' }).min(8, { message: 'error.input.password.min_length' }),
-  confirm_password: z.string({ required_error: 'error.input.password.required' }).min(8, { message: 'error.input.password.min_length' }),
-}).refine((data) => data.password === data.confirm_password, {
-  message: 'error.input.password_compare'
-})
+const schema = z
+  .object({
+    username: z
+      .string({ required_error: 'error.input.username.required' })
+      .min(3, { message: 'error.input.username.min_length' }),
+    email: z
+      .string({ required_error: 'error.input.email.required' })
+      .email({ message: 'error.input.email.invalid' }),
+    password: z
+      .string({ required_error: 'error.input.password.required' })
+      .min(8, { message: 'error.input.password.min_length' }),
+    confirm_password: z
+      .string({ required_error: 'error.input.password.required' })
+      .min(8, { message: 'error.input.password.min_length' }),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: 'error.input.password_compare',
+  })
 
 const validate_inputs = () => {
   const result = schema.safeParse(form)
@@ -87,11 +124,14 @@ const submit = async () => {
   try {
     const response = await api_client.post('/service/auth/signup', form)
     console.log(response.data)
+    set_cookie('access_token', response.data['access_token'])
+    set_cookie('refresh_token', response.data['refresh_token'])
+    window.location.href = '/'
   } catch (e) {
     if (e instanceof AxiosError) {
-      error_message.value = (e.response?.data as { detail?: string })?.detail || null    }
-  }
-  finally {
+      error_message.value = (e.response?.data as { detail?: string })?.detail || null
+    }
+  } finally {
     is_loading.value = false
   }
 }
