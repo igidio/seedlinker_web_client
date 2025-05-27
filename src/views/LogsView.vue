dfe1f1652928917ec53a61f36aef53e029b17a4a
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2025-05-26 23:55:27 -0400

    install chartjs

--- src/views/LogsView.vue
+++ src/views/LogsView.vue
@@ -10,20 +10,26 @@
     :title="$t('logs.title')"
   />
   <div class="flex flex-col gap-2">
-
     <div class="flex flex-col gap-2 flex-1">
       <LogsTable :logs="logs" />
     </div>
   </div>
+
+  <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
 </template>
 
 <script setup lang="ts">
 import ContentHeader from '@/components/project/global/ContentHeader.vue'
 import LogsTable from '@/components/project/logs/LogsTable.vue'
 import { useLogComposable } from '@/composables/log.composable'
-import { onMounted, provide} from 'vue'
+import { onMounted, provide } from 'vue'
+import { Bar } from 'vue-chartjs'
 
-const logComposable = useLogComposable();
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+const logComposable = useLogComposable()
 const { logs } = logComposable
 
 provide('get_content', logComposable.get_content)
@@ -35,4 +41,12 @@ provide('limit', logComposable.limit)
 onMounted(async () => {
   await logComposable.get_content()
 })
+
+const chartOptions = {
+  responsive: true,
+}
+const chartData = {
+  labels: ['January', 'February', 'March'],
+  datasets: [{ data: [40, 20, 12] }],
+}
 </script>
